99bfaca9609af6902d0d04be48289e0904c230ee
Arild M. Halvorsen <arild@monsegaard.no>
2017-10-15 11:55:50 +0200

    Laget ny fil

---
+++ system.php
@@ -0,0 +1,30 @@
+<?php
+/*
+File  Name: system.php
+File  URI: https://wiki.aktiv-kommune.no/
+Description: Used in Aktiv komunne API files
+Version: 0.1.0
+Author: Arild M. Halvorsen
+Author URI: http://about.me/arild
+*/
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+define( 'ROOT_DIR', dirname(__FILE__) );
+define( 'BASE_DIR', '/');
+define( "PORTICO_URL", "https://aktiv.fjell.kommune.no/");
+define( "PORTICO_IMAGES_URL", "https://aktiv.fjell.kommune.no/portico/bookingfrontend/");
+define( 'ORG_IMAGES_URL', 'http://aktiv.fjell.no/culture/images/org/');
+define( 'MUNICIPALITY_ID', '1299');
+define( 'LOCAL_COUNTRY_NAME', 'Norge' );
+define( 'CRON_JOB_OUTPUT', true);
+
+define( 'DB_HOST', '');  	    // server hostname
+define( 'DB_PORT', '5432');   // server port number
+define( 'DB_NAME', '');       // database name
+define( 'DB_USER', '');   		// username
+define( 'DB_PASS', '');       // password
+
+?>
